a4aa4360d59740d7d43a0e6ab442d4ebc72aa6d0
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2025-06-24 21:30:30 -0400

    refactor: move lspattach callback

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -19,77 +19,78 @@
 -- yoh      - toggle inlay hints
 -- <leader>gO - workspace symbols, like gO is for buffer symbols
 -- K        - popup docs window
+
+
+---@param ev any Event data
+local function lspattach_cb (ev)
+  ---@type vim.lsp.Client|nil
+  local client = vim.lsp.get_client_by_id(ev.data.client_id)
+  if client == nil then return end
+
+  vim.notify_once(string.format('%s %s attached.', '󰋼 ', client.name), vim.log.INFO)
+
+  if client:supports_method('textDocument/completion') then
+    vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
+  end
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true })
+
+  -- these quickly become annoying when on all the time so I set a vim-unimpaired style map
+  if client:supports_method('textDocument/inlayHint') then
+    vim.keymap.set('n', 'yoh', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      vim.notify(string.format('Show inlay hints set to %s', vim.lsp.inlay_hint.is_enabled()), vim.log.INFO)
+    end, { buffer = true })
+  end
+
+  if client:supports_method('workspace/symbol') then
+    vim.keymap.set('n', '<leader>gO', function()
+      ---@diagnostic disable-next-line: undefined-field
+      require('fzf-lua').lsp_live_workspace_symbols()
+    end, { buffer = true })
+  end
+
+  if client:supports_method('textDocument/formatting') then
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = ev.buf,
+      callback = function()
+        vim.lsp.buf.format({
+          bufnr = ev.buf,
+          id = client.id,
+          filter = function(c) return c.name ~= 'lua_ls' end,
+        })
+      end,
+    })
+  end
+
+  if client:supports_method('textDocument/documentHighlight') then
+    vim.cmd([[
+    autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    ]])
+  end
+
+  if client:supports_method('textDocument/hover') then
+    vim.keymap.set( 'n', 'K',
+    function() vim.lsp.buf.hover({ border = 'rounded', max_height = 10 }) end,
+    { buffer = true })
+  end
+
+  if client:supports_method('textDocument/foldingRange') then
+    local win = vim.api.nvim_get_current_win()
+    if vim.wo[win].foldmethod == 'marker' then
+      -- if user has fdm set to this, they want specific folds in this buffer so bail
+      return
+    end
+    vim.wo[win][0].foldmethod = 'expr'
+    vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+  end
+end
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my.lsp.attach', {}),
-  callback = function(args)
-    ---@type vim.lsp.Client|nil
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client == nil then return end
-
-    vim.notify_once(string.format('%s %s attached.', '󰋼 ', client.name), vim.log.INFO)
-
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
-    end
-
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true })
-
-    -- these quickly become annoying when on all the time so I set a vim-unimpaired style map
-    if client:supports_method('textDocument/inlayHint') then
-      vim.keymap.set('n', 'yoh', function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-        vim.notify(string.format('Show inlay hints set to %s', vim.lsp.inlay_hint.is_enabled()), vim.log.INFO)
-      end, { buffer = true })
-    end
-
-    if client:supports_method('workspace/symbol') then
-      vim.keymap.set('n', '<leader>gO', function()
-        ---@diagnostic disable-next-line: undefined-field
-        require('fzf-lua').lsp_live_workspace_symbols()
-      end, { buffer = true })
-    end
-
-    if client:supports_method('textDocument/formatting') then
-    --   not client:supports_method('textDocument/willSaveWaitUntil')
-    --   and client:supports_method('textDocument/formatting')
-    -- then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({
-            bufnr = args.buf,
-            id = client.id,
-            filter = function(c) return c.name ~= 'lua_ls' end,
-          })
-        end,
-      })
-    end
-
-    if client:supports_method('textDocument/documentHighlight') then
-      vim.cmd([[
-          autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
-          autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        ]])
-    end
-
-    if client:supports_method('textDocument/hover') then
-      vim.keymap.set( 'n', 'K',
-        function() vim.lsp.buf.hover({ border = 'rounded', max_height = 10 }) end,
-        { buffer = true }
-      )
-    end
-
-    if client:supports_method('textDocument/foldingRange') then
-      local win = vim.api.nvim_get_current_win()
-      if vim.wo[win].foldmethod == 'marker' then
-        -- if user has fdm set to this, they want specific folds in this buffer so bail
-        return
-      end
-      vim.wo[win][0].foldmethod = 'expr'
-      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
-    end
-  end,
+  callback = lspattach_cb,
 })
 
 vim.diagnostic.config({
