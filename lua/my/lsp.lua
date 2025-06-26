2c90a6d9eb1249dde24ae2960c3ad7294bd285bf
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2025-06-26 14:47:35 -0400

    lsp updates

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -1,14 +1,4 @@
--- Some mappings are done unconditionally in `$VIMRUNTIME/lua/vim/_defaults.lua`:
--- gra      - code (a)ction
--- grr      - (r)eferences
--- grn      - re(n)ame
--- gri      - (i)mplementation
--- gO       - document symbol (like help buffer gO binding)
--- i_CTRL-S - (s)ignature
--- an       -  expand selection, (a)dd (n)ode, using vim.lsp.buf.selection_range()
--- in       -  shrink selection, (i)n (n)ode, both can take a count too, like: 3van
--- <Tab>    - snippet jump forward if snippet active
--- <S-Tab>  - snippet jump backward if snippet active
+-- Some mappings are done unconditionally in `$VIMRUNTIME/lua/vim/_defaults.lua`: see `:h grr`
 --
 -- set here:
 -- gd       - (d)efinition
@@ -17,9 +7,7 @@
 -- g(       - incoming calls
 -- g)       - outgoing calls
 -- yoh      - toggle inlay hints
--- <leader>gO - workspace symbols, like gO is for buffer symbols
--- K        - popup docs window
-
+-- grO      - workspace symbols, like gO is for buffer symbols but using gr-prefix for lsp stuff
 
 ---@param ev any Event data
 local function lspattach_cb (ev)
@@ -29,12 +17,30 @@ local function lspattach_cb (ev)
 
   vim.notify_once(string.format('%s %s attached.', '󰋼 ', client.name), vim.log.INFO)
 
-  if client:supports_method('textDocument/completion') then
+  -- supported large majority of the time, no need to wrap these
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true })
+
+  if client:supports_method('textDocument/documentColor') then
     vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
   end
 
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true })
+  -- highlight hex colors like colorizer!
+  if client:supports_method('textDocument/highlight') then
+   vim.lsp.document_color.enable(true, ev.buf)
+ end
+
+  if client:supports_method('textDocument/typeDefinition') then
+    vim.keymap.set('n', 'gy', function() vim.lsp.buf.type_definition() end, { buffer = true })
+  end
+
+  if client:supports_method('callHierarchy/incomingCalls') then
+    vim.keymap.set('n', 'g(', function() vim.lsp.buf.incoming_calls() end, { buffer = true })
+  end
+
+  if client:supports_method('callHierarchy/outgoingCalls') then
+    vim.keymap.set('n', 'g)', function() vim.lsp.buf.outgoing_calls() end, { buffer = true })
+  end
 
   -- these quickly become annoying when on all the time so I set a vim-unimpaired style map
   if client:supports_method('textDocument/inlayHint') then
@@ -45,10 +51,7 @@ local function lspattach_cb (ev)
   end
 
   if client:supports_method('workspace/symbol') then
-    vim.keymap.set('n', '<leader>gO', function()
-      ---@diagnostic disable-next-line: undefined-field
-      require('fzf-lua').lsp_live_workspace_symbols()
-    end, { buffer = true })
+    vim.keymap.set('n', 'grO', function() require('fzf-lua').lsp_live_workspace_symbols() end, { buffer = true })
   end
 
   if client:supports_method('textDocument/formatting') then
@@ -71,16 +74,10 @@ local function lspattach_cb (ev)
     ]])
   end
 
-  if client:supports_method('textDocument/hover') then
-    vim.keymap.set( 'n', 'K',
-    function() vim.lsp.buf.hover({ border = 'rounded', max_height = 10 }) end,
-    { buffer = true })
-  end
-
   if client:supports_method('textDocument/foldingRange') then
     local win = vim.api.nvim_get_current_win()
     if vim.wo[win].foldmethod == 'marker' then
-      -- if user has fdm set to this, they want specific folds in this buffer so bail
+      -- if user has fdm set to this, it means they want specific folds in this buffer, so bail
       return
     end
     vim.wo[win][0].foldmethod = 'expr'
@@ -98,20 +95,21 @@ vim.diagnostic.config({
     border = 'single',
     header = 'Diagnostics',
     severity_sort = true,
-    source = true, -- show where diagnostic comes from
+    source = true,
   },
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      [vim.diagnostic.severity.WARN] = '󰀪 ',
-      [vim.diagnostic.severity.INFO] = '󰋽 ',
-      [vim.diagnostic.severity.HINT] = '󰌶 ',
+      [vim.diagnostic.severity.WARN]  = '󰀪 ',
+      [vim.diagnostic.severity.INFO]  = '󰋽 ',
+      [vim.diagnostic.severity.HINT]  = '󰌶 ',
     },
   },
   severity_sort = true,
   virtual_text = false,
 })
 
+--[[ my old way
 -- HACK: https://github.com/neovim/nvim-lspconfig/issues/3189
 local runtime_files = vim.api.nvim_get_runtime_file('', true)
 for k, v in ipairs(runtime_files) do
@@ -133,7 +131,7 @@ local luals_config = {
       workspace = {
         library = {
           vim.env.VIMRUNTIME,
-          -- TODO: inline this using vim.fn.glob(lazypath, '*')
+          -- TODO: inline this using vim.fn.glob(stdpath('data')..., '*')
           runtime_files,
         },
         checkThirdParty = false,
@@ -141,18 +139,84 @@ local luals_config = {
     },
   }
 }
-
--- merge-in plugin paths - we can't use '*' wildchar in .luarc.json, lua_ls doesn't support it
--- extends any config chain found
 vim.lsp.config('lua_ls', luals_config)
+--]]
 
-vim.lsp.enable({
-  'bashls',
-  'jsonls',
-  'lua_ls',
-  'taplo',
-  'vimls',
-  'vale_ls',
-  'yamlls',
-  'marksman',
+---@type vim.lsp.Config
+local lua_ls_config = {
+  root_markers = {
+    { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml' }, '.git'
+  },
+  on_init = function(client)
+  end,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  }
+}
+
+--[[[ from nvim-lspconfig docs:
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+        then
+          return
+        end
+      end
+
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most
+          -- likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Tell the language server how to find Lua modules same way as Neovim
+          -- (see `:h lua-module-load`)
+          path = {
+            'lua/?.lua',
+            'lua/?/init.lua',
+          },
+        },
+        -- Make the server aware of Neovim runtime files
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            vim.env.VIMRUNTIME
+            -- Depending on the usage, you might want to add additional paths
+            -- here.
+            -- '${3rd}/luv/library'
+            -- '${3rd}/busted/library'
+          }
+          -- Or pull in all of 'runtimepath'.
+          -- NOTE: this is a lot slower and will cause issues when working on
+          -- your own configuration.
+          -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+          -- library = {
+            --   vim.api.nvim_get_runtime_file('', true),
+            -- }
+          }
+        })
+      end,
+      settings = { Lua = {} }
 })
+--]]
+
+vim.lsp.config('lua_ls', lua_ls_config)
+vim.lsp.enable({ 'lua_ls' })
+
+-- vim.lsp.enable({
+  -- 'lua_ls',
+  -- 'bashls',
+  -- 'jsonls',
+  -- 'taplo',
+  -- 'vimls',
+  -- 'vale_ls',
+  -- 'yamlls',
+  -- 'marksman',
+-- })
