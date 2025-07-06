18065bc9d7dae7a341e1585187d168ea997a3855
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2025-07-06 08:13:01 -0400

    refactor and format lsp settings

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -12,31 +12,63 @@
 -- See what attached lsp client supports:
 -- :lua =vim.lsp.get_clients()[1].server_capabilities
 
+local function lspdetach_cb(args)
+  local client = vim.lsp.get_client_by_id(args.data.client_id)
+  -- Remove the autocommand to format the buffer on save, if it exists
+  if client:supports_method('textDocument/formatting') then
+    vim.api.nvim_clear_autocmds({
+      event = 'BufWritePre',
+      buffer = args.buf,
+    })
+  end
+end
+
 local function lspattach_cb(args)
-  ---@type vim.lsp.Client
   local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
   vim.notify_once(string.format('%s %s attached.', '󰋼 ', client.name), vim.log.INFO)
 
-  -- supported large majority of the time, no need to wrap these
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true })
+  if client:supports_method('textDocument/codeAction') then
+  end
 
-  if client:supports_method('textDocument/documentColor') then
-    -- does not work well with nvim default completeopt, unless 'noselect' is added
+  if client:supports_method('textDocument/definition') then
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
+  end
+  if client:supports_method('textDocument/declaration') then
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true })
+  end
+
+  if client:supports_method('textDocument/typeDefinition') then
+  end
+
+  if client:supports_method('textDocument/references') then
+  end
+
+  if client:supports_method('textDocument/rename') then
+  end
+
+  if client:supports_method('textDocument/documentSymbol') then
+  end
+
+  if client:supports_method('textDocument/completion') then
+    -- autotrigger doesn't work well with nvim default completeopt, unless 'noselect' is added
     vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
     vim.keymap.set('i', '<C-i>', function() vim.lsp.completion.get() end)
   end
 
   -- highlight hex colors like colorizer!
-  if client:supports_method('textDocument/documentHighlight') then vim.lsp.document_color.enable(true, args.buf) end
-
-  if client:supports_method('callHierarchy/incomingCalls') then
-    vim.keymap.set('n', 'g(', function() vim.lsp.buf.incoming_calls() end, { buffer = true })
+  if client:supports_method('textDocument/documentColor') then
+    vim.lsp.document_color.enable(true, args.buf)
   end
 
-  if client:supports_method('callHierarchy/outgoingCalls') then
-    vim.keymap.set('n', 'g)', function() vim.lsp.buf.outgoing_calls() end, { buffer = true })
+  if client:supports_method('textDocument/prepareCallHierarchy') then
+    if client:supports_method('callHierarchy/incomingCalls') then
+      vim.keymap.set('n', 'g(', function() vim.lsp.buf.incoming_calls() end, { buffer = true })
+    end
+
+    if client:supports_method('callHierarchy/outgoingCalls') then
+      vim.keymap.set('n', 'g)', function() vim.lsp.buf.outgoing_calls() end, { buffer = true })
+    end
   end
 
   -- these quickly become annoying when on all the time so I set a vim-unimpaired style map
@@ -51,21 +83,21 @@ local function lspattach_cb(args)
     vim.keymap.set('n', 'grO', function() require('fzf-lua').lsp_live_workspace_symbols() end, { buffer = true })
   end
 
-  -- if
-  --   not client:supports_method('textDocument/willSaveWaitUntil')
-  --   and client:supports_method('textDocument/formatting')
-  -- then
-  --   vim.api.nvim_create_autocmd('BufWritePre', {
-  --     buffer = args.buf,
-  --     callback = function()
-  --       vim.lsp.buf.format({
-  --         bufnr = args.buf,
-  --         id = client.id,
-  --         -- filter = function(c) return c.name ~= 'emmylua_ls' end,
-  --       })
-  --     end,
-  --   })
-  -- end
+  if
+      not client:supports_method('textDocument/willSaveWaitUntil')
+      and client:supports_method('textDocument/formatting')
+  then
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format({
+          bufnr = args.buf,
+          id = client.id,
+          -- filter = function(c) return c.name ~= 'lua_ls' end,
+        })
+      end,
+    })
+  end
 
   if client:supports_method('textDocument/documentHighlight') then
     vim.cmd([[
@@ -90,6 +122,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = lspattach_cb,
 })
 
+vim.api.nvim_create_autocmd('LspDetach', {
+  group = vim.api.nvim_create_augroup('my.lsp.detach', {}),
+  callback = lspdetach_cb,
+})
+
 vim.diagnostic.config({
   float = {
     border = 'single',
@@ -110,7 +147,7 @@ vim.diagnostic.config({
 })
 
 ---@type vim.lsp.Config
-local emmylua_ls_config = {
+local lua_ls_config = {
   on_init = function(client)
     -- do nothing if not in a proper workspace (no marker, even .git, found)
     if not client.workspace_folders then return end
@@ -128,11 +165,10 @@ local emmylua_ls_config = {
     })
   end,
 }
--- merge the `on_init()` method to the config
-vim.lsp.config('emmylua_ls', emmylua_ls_config)
+vim.lsp.config('lua-language-server', lua_ls_config)
 
 vim.lsp.enable({
-  'emmylua_ls',
+  'lua-language-server',
   'vscode-json-language-server',
   -- 'taplo',
   -- 'vimls',
