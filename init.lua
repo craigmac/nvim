706ec5758ad5b8b4f8e9b5cf83a7211b122542f2
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2025-08-31 21:40:52 -0400

    Change terminal mode cursor to green

--- init.lua
+++ init.lua
@@ -397,4 +397,5 @@ vim.cmd([[
   " default link to Statement is just bold and hard to spot diff line markers quickly,
   " instead make them cyan like lazygit does
   hi! diffLine guifg=cyan
+  hi! TermCursor gui=none guibg=lightgreen guifg=white
 ]])
