25179d07ef9e7cfb12aef7bd29090f0054efd3b6
Craig MacEachern <wg51@queensu.ca>
2025-09-17 12:12:59 -0400

    set terminal buffer colorscheme to dimidium if gui running

--- lua/my/gui.lua
+++ lua/my/gui.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
-  vim.notify('Neovide detected. Loading settings...')
-  vim.cmd.tcd('~/.config/nvim')
+  -- vim.notify('Neovide detected. Loading settings...')
+  vim.cmd.tcd('~')
   -- padding is wasted space
   vim.g.neovide_padding_top = 0
   vim.g.neovide_padding_bottom = 0
@@ -11,8 +11,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_short_animation_length = 0
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_cursor_animate_command_line = false
-  -- if system doesn't have each of these fonts it will error out
-  vim.o.guifont = 'Adwaita Mono,Cascadia Mono,Inconsolata:14'
   -- fullscreen binding using typical F11
   vim.keymap.set('n', '<F11>', ':<C-u>let g:neovide_fullscreen = !g:neovide_fullscreen<CR>')
   -- <C-[> is distinct from <Esc> in GUI
@@ -40,3 +38,38 @@ vim.api.nvim_create_autocmd('UIEnter', {
   group = guis_augroup,
 })
 
+-- set terminal buffers to use 'Dimidium' palette
+-- NOTE: terminal buffer background/foreground can't be set easily - there's no dedicated TermNormal group, it just
+-- inherits from Normal, so we need to use `:h 'winhighlight` in coordination with autocmds to set/unset bg/fg in terminal
+-- buffers.
+--
+-- TODO:
+-- vim.
+
+if vim.fn.has('gui_running') then
+
+  local ns_id = vim.api.nvim_create_namespace('my.terminal.colours')
+-- TODO: TermOpen, WinEnter autocmds to set this highlights for terminal buffers
+-- vim.api.nvim_set_hl(ns_id, 'Terminal', { bg = '#141414', fg = '#bab7b6 })
+-- vim.api.nvim_set_hl(ns_id, 'Visual' { bg = '#8db8e5', fg = '#000000' })
+--
+  -- use bright green cursor defined by Dimidium
+  vim.api.nvim_set_hl(0, 'TermCursor', { bg = '#37e57b', fg = '#000000' })
+
+  vim.g.terminal_color_0 = '#000000'
+  vim.g.terminal_color_1 = '#cf494c'
+  vim.g.terminal_color_2 = '#60b442'
+  vim.g.terminal_color_3 = '#db9c11'
+  vim.g.terminal_color_4 = '#0575d8'
+  vim.g.terminal_color_5 = '#af5ed2'
+  vim.g.terminal_color_6 = '#1db6bb'
+  vim.g.terminal_color_7 = '#bab7b6'
+  vim.g.terminal_color_8 = '#817e7e'
+  vim.g.terminal_color_9 = '#ff643b'
+  vim.g.terminal_color_10 = '#37e57b'
+  vim.g.terminal_color_11 = '#fccd1a'
+  vim.g.terminal_color_12 = '#688dfd'
+  vim.g.terminal_color_13 = '#ed6fe9'
+  vim.g.terminal_color_14 = '#32e0fb'
+  vim.g.terminal_color_15 = '#dee3e4'
+end
