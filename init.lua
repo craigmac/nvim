7d5fd41de20d853e3e7db04bb42d487ebfa04ea3
Craig MacEachern <wg51@queensu.ca>
2025-09-17 12:12:23 -0400

    refactor how packages setup
    keep it as simple as possible

--- init.lua
+++ init.lua
@@ -6,6 +6,15 @@ require('my.autocmds')
 require('my.keymaps')
 require('my.commands')
 require('my.colors')
-require('my.packages.init')
 require('my.gui')
 require('my.terminal')
+
+require('my.packages')
+require('my.plugins.fzf-lua')
+require('my.plugins.smart-splits')
+require('my.plugins.nvim-lspconfig')
+-- require('my.plugins.mini-completion')
+-- require('my.plugins.gitsigns')
+-- require('my.plugins.nvim-surround')
+
+
---
+++ lua/my/packages.lua
@@ -0,0 +1,18 @@
+vim.cmd.packadd('netrw')
+vim.cmd.packadd('nohlsearch')
+
+-- first clone https://github.com/savq/paq-nvim
+require('paq')({
+  'savq/paq-nvim',
+  'tpope/vim-repeat',
+  'tpope/vim-rsi',
+  'tpope/vim-unimpaired',
+  'neovim/nvim-lspconfig',
+  'ibhagwan/fzf-lua',
+  'glacambre/firenvim', -- requires :call firenvim#install(0) after an update
+  'kylechui/nvim-surround',
+  'mrjones2014/smart-splits.nvim',
+  'lewis6991/gitsigns.nvim',
+  'catgoose/nvim-colorizer.lua',
+})
+
