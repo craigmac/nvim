db6afa208671040ec1505fd2175f9abc8e1becc3
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2025-09-19 21:03:34 -0400

    syncing

--- init.lua
+++ init.lua
@@ -14,25 +14,24 @@ require('my.colors')
 vim.cmd.packadd('nohlsearch')
 
 vim.pack.add({
-  'https://github.com/ibhagwan/fzf-lua',
-  'https://github.com/mrjones2014/smart-splits.nvim',
-  'https://github.com/tpope/vim-rsi',
-  'https://github.com/tpope/vim-endwise',
-  'https://github.com/tpope/vim-fugitive',
-  'https://github.com/tpope/vim-unimpaired',
-  'https://github.com/tpope/vim-repeat',
-  'https://github.com/tpope/vim-rhubarb',
-  'https://github.com/tpope/vim-dispatch',
-  'https://github.com/kylechui/nvim-surround',
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', version = 'main' },
-  'https://github.com/andrewferrier/debugprint.nvim',
-  -- `gl|L`, `gl/`
-  'https://github.com/tommcdo/vim-lion',
-  -- run `:call firenvim#install(0)` if this updates and on first install
-  'https://github.com/glacambre/firenvim',
-  'https://github.com/romainl/vim-qf',
-  'https://github.com/lewis6991/gitsigns.nvim',
+    'https://github.com/ibhagwan/fzf-lua',
+    'https://github.com/mrjones2014/smart-splits.nvim',
+    'https://github.com/tpope/vim-rsi',
+    'https://github.com/tpope/vim-endwise',
+    'https://github.com/tpope/vim-fugitive',
+    'https://github.com/tpope/vim-unimpaired',
+    'https://github.com/tpope/vim-repeat',
+    'https://github.com/tpope/vim-rhubarb',
+    'https://github.com/tpope/vim-dispatch',
+    'https://github.com/kylechui/nvim-surround',
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter',             version = 'main' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', version = 'main' },
+    'https://github.com/andrewferrier/debugprint.nvim',
+    'https://github.com/tommcdo/vim-lion',
+    -- run `:call firenvim#install(0)` if this updates and on first install
+    'https://github.com/glacambre/firenvim',
+    'https://github.com/romainl/vim-qf',
+    'https://github.com/lewis6991/gitsigns.nvim',
 })
 
 require('plugins.fzf-lua')
--- lua/my/options.lua
+++ lua/my/options.lua
@@ -1,17 +1,18 @@
 -- special characters and display
 vim.o.fillchars =
-  'eob:-,diff: ,fold: ,foldclose:▶,foldopen:▼,horiz: ,horizdown: ,horizup: ,vert:│,vertleft:│,vertright:│,verthoriz:│,lastline:⋯,msgsep:─,trunc:⋯,truncrl:⋯'
+'eob:-,diff: ,fold: ,foldclose:▶,foldopen:▼,horiz: ,horizdown: ,horizup: ,vert:│,vertleft:│,vertright:│,verthoriz:│,lastline:⋯,msgsep:─,trunc:⋯,truncrl:⋯'
 vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25-TermCursor'
-vim.o.listchars = 'eol:¬,tab:⇥ ,trail:░,extends:»,precedes:«,nbsp:⍽'
+vim.o.listchars = 'eol: ,tab:⇥ ,trail:█,extends:»,precedes:«,nbsp:⍽'
 vim.o.list = true
-vim.o.winborder = 'rounded'
+-- some fonts don't render the rounded border well
+vim.o.winborder = 'single'
 vim.o.foldtext = ''
 vim.o.foldlevelstart = 99
 vim.o.showmode = false
 
 require('vim._extui').enable({
-  enable = true,
-  msg = { target = 'msg', timeout = 4000 },
+    enable = true,
+    msg = { target = 'msg', timeout = 4000 },
 })
 
 -- no wrap, but if we do enable wrap, use these wrap-related settings
@@ -29,12 +30,12 @@ vim.o.smartcase = true
 vim.o.inccommand = 'split'
 vim.o.pumheight = 10
 vim.o.shortmess = vim.o.shortmess
-  .. table.concat({
-    'c', -- no 'match 1 of 2' etc. messages when scrolling through completions
-    's', -- no 'search hit BOTTOM...' messages and don't show 'W' for wrapped before [1/3]
-    'q', -- no 'recording @q' when recording macro
-    -- 'S', -- no [1/5] search count shown
-  })
+    .. table.concat({
+        'c', -- no 'match 1 of 2' etc. messages when scrolling through completions
+        's', -- no 'search hit BOTTOM...' messages and don't show 'W' for wrapped before [1/3]
+        'q', -- no 'recording @q' when recording macro
+        -- 'S', -- no [1/5] search count shown
+    })
 vim.o.wildcharm = vim.keycode('<C-z>'):byte()
 vim.o.wildmode = 'noselect:lastused,full'
 
@@ -49,11 +50,12 @@ vim.o.statusline = '%!v:lua.My.StatusLine()'
 vim.o.tabline = '%!v:lua.My.TabLine()'
 vim.o.winbar = '%!v:lua.My.Winbar()'
 
--- editing
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.softtabstop = -1 -- use shiftwidth
-vim.o.spelllang = 'canadian'
+-- tabs and spaces, `:h usr_30.txt`
+vim.o.shiftwidth = 4   -- columns per level of (auto)indentation, 0 uses `&ts`
+vim.o.softtabstop = -1 -- use value of &sw
+
+-- spelling
+-- vim.o.spelllang = 'canadian'
 vim.o.spelloptions = 'camel,noplainbuffer'
 vim.o.spellsuggest = 'fast,5'
 
