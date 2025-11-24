6c0ee065725ab9ff739339f3cc01b0f14be03944
C.D. MacEachern <wg51@queensu.ca>
2025-11-24 11:57:09 -0500

    mason, easy align

--- init.lua
+++ init.lua
@@ -21,16 +21,17 @@ vim.api.nvim_create_autocmd('OptionSet', {
 -- put configurations for these in `./after/plugins`, so plugin runtimes have been sourced
 vim.pack.add({
   'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/mason-org/mason.nvim',
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', version = 'main' },
   'https://github.com/ibhagwan/fzf-lua',
   'https://github.com/mrjones2014/smart-splits.nvim',
-  -- $VIMRUNTIME/lua/vim/_defaults.lua did not implement: `>p` `=p` (and variants), `yo<key>`, `]e` `[e`
+  -- $VIMRUNTIME/lua/vim/_defaults.lua is missing: `>p` `=p` (and variants), `yo<key>`, `]e` `[e`
   'https://github.com/tpope/vim-unimpaired',
   'https://github.com/tpope/vim-repeat',
   'https://github.com/tpope/vim-rsi',
   'https://github.com/kylechui/nvim-surround',
-  'https://github.com/tommcdo/vim-lion',
+  'https://github.com/junegunn/vim-easy-align',
   'https://github.com/glacambre/firenvim',
   'https://github.com/romainl/vim-qf',
   'https://github.com/tpope/vim-fugitive',
